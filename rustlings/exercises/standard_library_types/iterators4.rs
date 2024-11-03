4ab5e9a0c6e66e6dce6ee46c2a8ba06621e49a56
Oleksandr Igumnov <igumssukr@gmail.com>
2024-11-03 18:28:49 +0200

    Solved rustlings 'standard_library_types' topic

--- rustlings/exercises/standard_library_types/iterators4.rs
+++ rustlings/exercises/standard_library_types/iterators4.rs
@@ -1,8 +1,7 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    (1u64..(num + 1)).fold(1, |accumulator, item| accumulator * item)
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
